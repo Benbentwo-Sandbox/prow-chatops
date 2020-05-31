07038b45bb97a2cf49d2018899ede88af5417a92
Benjamin Smith <ben.smith.developer@gmail.com>
2020-05-31 08:23:57 -0400

    githubevent

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import (
 	actions "github.com/sethvargo/go-githubactions"
 	"os"
+	// "github.com/sirupsen/logrus"
+	// "k8s.io/test-infra/prow/config"
+	// "k8s.io/test-infra/prow/github"
+	// "k8s.io/test-infra/prow/github/fakegithub"
+	// "k8s.io/test-infra/prow/labels"
+	// "k8s.io/test-infra/prow/plugins"
 )
 
 func main() {
@@ -14,4 +20,5 @@ func main() {
 
 	actions.AddMask(fruit)
 	actions.AddMask(githubToken)
+	actions.Warningf("GithubEvent: %s", actions.GetInput("githubevent"))
 }
