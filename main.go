f826458b9b4dddf1eae48da55fba8aad737bc22a
Benjamin Smith <ben.smith.developer@gmail.com>
2020-05-31 09:36:18 -0400

    iniyisl pstdr

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	actions "github.com/sethvargo/go-githubactions"
 	// "github.com/sirupsen/logrus"
 	// "k8s.io/test-infra/prow/config"
-	// "k8s.io/test-infra/prow/github"
+	"k8s.io/test-infra/prow/github"
 	// "k8s.io/test-infra/prow/github/fakegithub"
 	// "k8s.io/test-infra/prow/labels"
 	// "k8s.io/test-infra/prow/plugins"
@@ -12,5 +13,12 @@ import (
 
 func main() {
 	event := actions.GetInput("githubevent")
-	actions.Warningf("Event: %s", event)
+	actions.Debugf("Event: %s", event)
+	var issueCommentEvent github.IssueCommentEvent
+
+	err := json.Unmarshal([]byte(event), &issueCommentEvent)
+	if err != nil {
+		actions.Fatalf("Couldn't Parse the github event!")
+	}
+
 }
